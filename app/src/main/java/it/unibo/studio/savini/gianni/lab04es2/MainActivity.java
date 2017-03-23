7c0a2e7ebc466283de784e1e7b5b66146fd30763
Gianni Savini <gianni.savini@studio.unibo.it>
2017-03-23 16:06:26 +0100

    Simple exaple of use permission for a GPS sensor

---
+++ app/src/main/java/it/unibo/studio/savini/gianni/lab04es2/MainActivity.java
@@ -0,0 +1,59 @@
+package it.unibo.studio.savini.gianni.lab04es2;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private TextView txvProvider;
+    private TextView txvLatitude;
+    private TextView txvLongitude;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txvProvider = (TextView) findViewById(R.id.txv_provider);
+        txvLatitude = (TextView) findViewById(R.id.txv_latitude);
+        txvLongitude = (TextView) findViewById(R.id.txv_longitude);
+
+        register();
+    }
+
+    private void register() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //Per fare ciò serve un permesso particolare
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, MainActivity.this);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //Il warning è dato dalla gestione non efficace delle stringhe
+        txvProvider.setText("Provider: " + location.getProvider());
+        txvLatitude.setText(String.format("Latitudine: %f", location.getLatitude()));
+        txvLongitude.setText(String.format("Longitudine: %f", location.getLongitude()));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(MainActivity.this, provider + "abilitato", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(MainActivity.this, provider + "disabilitato", Toast.LENGTH_SHORT).show();
+    }
+}
